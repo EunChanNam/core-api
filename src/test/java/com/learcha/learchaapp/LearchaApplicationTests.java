4d750e6fc1845b221e229605751a2c8db521649b
devbloo <zpswl45@naver.com>
2023-01-27 19:22:36 +0900

    chore: exclude contents load test

--- src/test/java/com/learcha/learchaapp/LearchaApplicationTests.java
+++ src/test/java/com/learcha/learchaapp/LearchaApplicationTests.java
@@ -1,13 +1,8 @@
 package com.learcha.learchaapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LearchaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
