e91a69a2fda19897d6757d1ce57b66997846ac8c
devbloo <zpswl45@naver.com>
2023-01-30 20:49:21 +0900

    feat: add exception handling

--- src/main/java/com/learncha/api/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/learncha/api/common/exception/handler/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.learncha.api.common.exception.handler;
 
 import com.learncha.api.common.error.ErrorResponse;
+import com.learncha.api.common.exception.AlreadyAuthenticatedEmail;
 import com.learncha.api.common.exception.EntityNotFoundException;
 import com.learncha.api.common.exception.InvalidParamException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -31,5 +33,17 @@ public class GlobalExceptionHandler {
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         log.debug(ex.getMessage());
         return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex.getFieldError());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(AlreadyAuthenticatedEmail.class)
+    public ErrorResponse handleAlreadyAuthenticatedException(EntityNotFoundException ex) {
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 }
