3c43c4f22004d2e439ca64f169c02e77ab9c1924
devbloo <zpswl45@naver.com>
2023-01-30 13:56:43 +0900

    refactor: 이메일 발송 로직 수정
    ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    # <타입> 리스트
    #   feat    : 기능 (새로운 기능)
    #   fix     : 버그 (버그 수정)
    #   refactor: 리팩토링
    #   style   : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs    : 문서 (문서 추가, 수정, 삭제)
    #   test    : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 스크립트 수정 등)
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- src/main/java/com/learncha/api/auth/service/AuthService.java
+++ src/main/java/com/learncha/api/auth/service/AuthService.java
@@ -2,9 +2,11 @@ package com.learncha.api.auth.service;
 
 import com.learncha.api.auth.domain.Member;
 import com.learncha.api.auth.domain.Member.AuthType;
+import com.learncha.api.auth.domain.Member.Status;
 import com.learncha.api.auth.repository.MemberRepository;
 import com.learncha.api.auth.web.AuthDto.SignUpRequest;
 import com.learncha.api.auth.web.AuthDto.SignUpResponse;
+import com.learncha.api.common.exception.AlreadyAuthenticatedEmail;
 import com.learncha.api.common.exception.InvalidParamException;
 import java.util.Random;
 import javax.mail.Message.RecipientType;
@@ -38,24 +40,63 @@ public class AuthService {
         Member initMember = memberRepository.findByEmail(email)
             .orElseThrow(() -> new InvalidParamException("해당 Email로 인증된 사용자가 없습니다."));
 
-        Member authenticatedMember = initMember.updateEmailAuthenticatedUser(signUpRequest, encodedPassword);
+        Member authenticatedMember = initMember.updateEmailAuthenticatedUser(signUpRequest,
+            encodedPassword);
 
         memberRepository.save(authenticatedMember);
 
-        return SignUpResponse.builder()
-            .email(email)
+        return SignUpResponse.builder().email(email)
             .memberToken(authenticatedMember.getMemberToken())
-            .authType(authenticatedMember.getAuthType().getDescription())
-            .build();
+            .authType(authenticatedMember.getAuthType().getDescription()).build();
     }
 
-    public void emailAuthentication(String email) throws Exception {
-        MimeMessage message = createMessage(Member.createInitEmailMember(email, AuthType.EMAIL), email);
+    public void emailAuthentication(String email) {
+        memberRepository.findByEmail(email)
+            .ifPresentOrElse(
+                member -> {
+                    String memberStatus = member.getStatus().getDescription();
+                    if(memberStatus.equals(Status.AUTHENTICATED.getDescription())) {
+                        throw new AlreadyAuthenticatedEmail();
+                    } else if(memberStatus.equals(Status.NEED_AUTHENTICATED.getDescription())) {
+                        Member reMember = reSendAuthCodeToEmail(member, email);
+                        memberRepository.save(reMember);
+                    }
+
+                },
+                () -> {
+                    Member member = sendFirstAuthCodeToEmail(email);
+                    memberRepository.save(member);
+                });
+    }
+
+    private Member sendFirstAuthCodeToEmail(String email) {
+        Member initMember = Member.createInitEmailMember(email, AuthType.EMAIL);
+        MimeMessage message = null;
+
+        try {
+            message = createMessage(initMember, email);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        javaMailSender.send(message); return initMember;
+    }
+
+    private Member reSendAuthCodeToEmail(Member member, String email) {
+        MimeMessage message = null;
+
+        try {
+            message = createMessage(member, email);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+
         javaMailSender.send(message);
+        return member;
     }
 
     public boolean getAuthResult(String authCode, String email) {
-        Member member = memberRepository.findByEmailAndAuthenticationCode(email, authCode)
+        Member member = memberRepository.findByEmail(email)
             .orElseThrow(InvalidParamException::new);
 
         String savedAuthCode = member.getAuthenticationCode();
@@ -68,8 +109,7 @@ public class AuthService {
     }
 
     public boolean isAvailableEmail(String email) {
-        boolean res = memberRepository.existsMemberByEmail(email);
-        if(! res) {
+        boolean res = memberRepository.existsMemberByEmail(email); if(! res) {
             Member member = Member.createInitEmailMember(email, AuthType.EMAIL);
             memberRepository.save(member);
         }
@@ -77,15 +117,13 @@ public class AuthService {
         return res;
     }
 
-
     private String createAuthCode() {
-        StringBuffer key = new StringBuffer();
-        Random rnd = new Random();
+        StringBuffer key = new StringBuffer(); Random rnd = new Random();
 
-        for (int i = 0; i < 8; i++) { // 인증코드 8자리
+        for(int i = 0; i < 8; i++) { // 인증코드 8자리
             int index = rnd.nextInt(3); // 0~2 까지 랜덤
 
-            switch (index) {
+            switch(index) {
                 case 0:
                     key.append((char) ((int) (rnd.nextInt(26)) + 97));
                     //  a~z  (ex. 1+97=98 => (char)98 = 'b')
@@ -99,37 +137,26 @@ public class AuthService {
                     // 0~9
                     break;
             }
-        }
-        return key.toString();
+        } return key.toString();
     }
 
     private MimeMessage createMessage(Member member, String to) throws Exception {
-        String authCode = createAuthCode();
-        member.setAuthenticationCode(authCode);
+        String authCode = createAuthCode(); member.setAuthenticationCode(authCode);
 
-        MimeMessage  message = javaMailSender.createMimeMessage();
+        MimeMessage message = javaMailSender.createMimeMessage();
 
         message.addRecipients(RecipientType.TO, to);//보내는 대상
         message.setSubject("이메일 인증 테스트");//제목
 
-        String msgg="";
-        msgg+= "<div style='margin:20px;'>";
-        msgg+= "<h1> 안녕하세요 learncha 입니다. </h1>";
-        msgg+= "<br>";
-        msgg+= "<p>아래 코드를 복사해 입력해주세요<p>";
-        msgg+= "<br>";
-        msgg+= "<p>감사합니다.<p>";
-        msgg+= "<br>";
-        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg+= "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
-        msgg+= "<div style='font-size:130%'>";
-        msgg+= "CODE : <strong>";
-        msgg+= authCode+"</strong><div><br/> ";
-        msgg+= "</div>";
+        String msgg = ""; msgg += "<div style='margin:20px;'>";
+        msgg += "<h1> 안녕하세요 learncha 입니다. </h1>"; msgg += "<br>"; msgg += "<p>아래 코드를 복사해 입력해주세요<p>";
+        msgg += "<br>"; msgg += "<p>감사합니다.<p>"; msgg += "<br>";
+        msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg += "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
+        msgg += "<div style='font-size:130%'>"; msgg += "CODE : <strong>";
+        msgg += authCode + "</strong><div><br/> "; msgg += "</div>";
         message.setText(msgg, "utf-8", "html");
-        message.setFrom(new InternetAddress(email,"learncha"));
-
-        memberRepository.save(member);
+        message.setFrom(new InternetAddress(email, "learncha"));
 
         return message;
     }
