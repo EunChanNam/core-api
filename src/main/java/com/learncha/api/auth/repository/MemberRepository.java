74c0d0e91129cef6d4c922439f461eb4ec225f79
devbloo <zpswl45@naver.com>
2023-02-04 13:17:41 +0900

    fix: jpa method

--- src/main/java/com/learncha/api/auth/repository/MemberRepository.java
+++ src/main/java/com/learncha/api/auth/repository/MemberRepository.java
@@ -5,17 +5,17 @@ import com.learncha.api.auth.domain.Member.Status;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
     Optional<Member> findByEmailAndStatusIsNot(String email, Status status);
     @Query(
-        value = "select * from member as m where m.email = email and m.status != 'Deleted'",
+        value = "select * from member as m where m.email = :email and m.status != 'Deleted'",
         nativeQuery = true
     )
-    Optional<Member> findByEmailAndStatusIsNotDeleted(String email);
+    Optional<Member> findByEmailAndStatusIsNotDeleted(@Param(value = "email") String email);
     boolean existsMemberByEmail(String email);
-
     boolean findByPasswordAndStatusIsNot(String password, Status status);
 }
